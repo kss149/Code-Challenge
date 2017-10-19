ad5c7a78c6ddddd0c8f9e7e51d3e10f1cf37efb8
kosara <kos.bos@gmail.com>
2017-10-19 19:18:22 +0300

    Add files via upload

--- train.py
+++ train.py
@@ -4,9 +4,11 @@ import gensim
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+#Code is taken from https://radimrehurek.com/gensim/tut1.html
+
 #---------------------------------------------------Load Corpus from JSON files---------------------------------------------------#
 all_json_files = []
-training_data = []
+documentBodies = []
 training_data_size = int(sys.argv[1])-1
 
 # this finds the json files
@@ -20,29 +22,29 @@ for js in all_json_files:
     while(training_data_size!=0):
         with open(os.path.join('json/', js)) as json_file:
             json_text = json.load(json_file)
-            training_data.append(json_text['m_szDocBody'])
+            documentBodies.append(json_text['m_szDocBody'])
         training_data_size-=1
-training_data = training_data[21:]
+documentBodies = documentBodies[21:]
 
 #------------------------------------------------Preprocess Corpus of Document Bodies----------------------------------------------#
 # remove common words and tokenize
 stoplist = set('for a of the and to in'.split())
-documentBodies = [[word for word in training_data_one_document.lower().split() if word not in stoplist]
-         for training_data_one_document in training_data]
+training_data = [[word for word in documentBody.lower().split() if word not in stoplist]
+         for documentBody in documentBodies]
 
 # remove words that appear only once
 from collections import defaultdict
 frequency = defaultdict(int)
-for documentBody in documentBodies:
-    for token in documentBody:
+for training_vector in training_data:
+    for token in training_vector:
         frequency[token] += 1
-documentBodies = [[token for token in documentBody if frequency[token] > 1]
-         for documentBody in documentBodies]
+training_data = [[token for token in training_vector if frequency[token] > 1]
+         for training_vector in training_data]
 
-dictionary = gensim.corpora.Dictionary(documentBodies)
+dictionary = gensim.corpora.Dictionary(training_data)
 dictionary.save('corpus.dict')
 
-corpus = [dictionary.doc2bow(documentBody) for documentBody in documentBodies]
+corpus = [dictionary.doc2bow(training_vector) for training_vector in training_data]
 gensim.corpora.MmCorpus.serialize('corpus.mm', corpus)  # store to disk, for later use
 
 #------------------------------------------------------Train and Save model------------------------------------------------------#
